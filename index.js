bc76a3aeb9ca0c48a0437445a888552e02c302e2
MeliiZa <melissazandman@icloud.com>
2024-05-24 22:37:56 -0500

    Add animation to the card and improve animation for the thank you image

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Function to apply GSAP animations
+    // This function is to apply the GASP animations
     function applyAnimations() {
-        gsap.to(".social-icons", { x: 30 });
-        gsap.to(".thankyou", { x: -130 });
+        gsap.to(".social-icons", {
+            x: 30,
+            scrollTrigger: {
+                trigger: ".social-icons",
+                start: "top 80%", //I have added this so the animation starts only when the top is 80% in view of the viewport
+                end: "bottom 20%",
+                toggleActions: "play none none reverse",
+            }
+        });
+
+        const thankYouTimeline = gsap.timeline({
+            defaults: { duration: 0.75 },
+            scrollTrigger: {
+                trigger: ".thankyou",
+                start: "top 80%",
+                end: "bottom 20%",
+                toggleActions: "play none none reverse",
+            }
+        });
+
+        thankYouTimeline.fromTo(".thankyou", { scale: 0 }, { scale: 1 });
     }
 
-    // Check if the viewport width is greater than 767px
+    const cardTimeline = gsap.timeline({
+        defaults: { duration: 0.75 },
+        scrollTrigger: {
+            trigger: ".card",
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    cardTimeline.fromTo(".card", { scale: 0 }, { scale: 1 });
+
+    // Apply animations based on viewport width
     if (window.innerWidth > 767) {
         applyAnimations();
     }
 
-    // Event listener for window resize to reapply animations if necessary
+    // Reapply animations on window resize if necessary
     window.addEventListener("resize", () => {
         if (window.innerWidth > 767) {
             applyAnimations();
         } else {
             gsap.set(".social-icons", { x: 0 });
-            gsap.set(".thankyou", { x: 0 });
+            gsap.set(".thankyou", { scale: 1 });
         }
     });
 });
-
-
-
